ad60f957f486052a0a3eecbfb8cbedb05d13772c
slimek <slime573@gmail.com>
2016-03-22 23:36:43 +0800

    Add Play scene, with timer bar and gesture buttons.

---
+++ Assets/Scripts/PlayScene.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayScene : MonoBehaviour
+{
+    public UILabel statusLabel;
+    public UIProgressBar playTimerBar;
+
+    
+    void Start()
+    {
+        statusLabel.text = "Ready";
+
+        StartCoroutine( PlayTimer() );
+    }
+
+    private readonly float PlayDuration = 2;
+    private float playClock = 0;
+
+    IEnumerator PlayTimer()
+    {
+        while ( playClock < PlayDuration )
+        {
+            playClock += Time.deltaTime;
+            playTimerBar.value = playClock / PlayDuration;
+
+            yield return null;
+        }
+    }
+
+    public void OnRockClick()
+    {
+    }
+
+    public void OnPaperClick()
+    {
+    }
+
+    public void OnScissorsClick()
+    {
+    }
+}
